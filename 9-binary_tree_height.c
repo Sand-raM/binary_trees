bb921f0d7bde64a7a943b6d26be98226d40946ac
Sand-raM <umurazawigasabo@gmail.com>
2024-01-30 08:34:52 -0800

    binarytrees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,17 +9,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		int left = 0, right = 0;
+		size_t ltf = 0, rgt = 0;
 
-		if (tree->right)
-			right = 1 + binary_tree_height(tree->right);
-		if (tree->left)
-			left = 1 + binary_tree_height(tree->left);
-		if (left > right)
-			return (left);
-		else
-			return (right);
-		else
-			return (0);
+		lft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rgt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((lft > rgt) ? lft : rgt);
 	}
+	return (0);
 }
