4f78b02fc2c1407465857034103064b5454094c7
Sand-raM <umurazawigasabo@gmail.com>
2024-01-31 07:53:56 -0800

    binary trees

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,24 +10,19 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *auxi, *pivot;
 
-	if (tree == NULL || tree->left == NULL)
+	if (tree == NULL)
 		return (NULL);
-
-	auxi = tree->left;
-	pivot = auxi->right;
-	auxi->right = tree;
-	tree->left = pivot;
-	if (pivot != NULL)
-		pivot->parent = tree;
-	pivot = tree->parent;
-	tree->parent = auxi;
-	auxi->parent = pivot;
-	if (pivot != NULL)
+	if (tree->left)
 	{
-		if (pivot->left == tree)
-			pivot->left = auxi;
-		else
-			pivot->right = auxi;
+		pivot = tree->left->right;
+		auxi = tree->left;
+		auxi->parent = tree->parent;
+		auxi->right = tree;
+		tree->parent = auxi;
+		tree->left = pivot;
+		if (pivot)
+			pivot->parent = tree;
+		return (auxi);
 	}
-	return (pivot);
+	return (NULL);
 }
